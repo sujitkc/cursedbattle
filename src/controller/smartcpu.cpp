16ea6e54e2690a28162b04cfd8b5fb0122618690
sujitkc <sujitkc@gmail.com>
2014-06-13 21:46:24 +0530

    committing smartcpu

--- src/controller/smartcpu.cpp
+++ src/controller/smartcpu.cpp
@@ -1,268 +1,195 @@
 #include <stdlib.h>
+#include <fstream>
 #include <time.h>
-#include <curses.h>
-#include <iostream>
-
 #include <algorithm>
 
-#include "user.h"
-#include "board.h"
 #include "vessel.h"
-
-#include "gui.h"
-#include "keyboard.h"
+#include "board.h"
+#include "smartcpu.h"
 
 using namespace std;
-User::User(const string n, Board* b, GUIBoard* gb, Board* e, GUIBoard* ge)
-  : name(n), myBoard(b), guiBoard(gb), enemyBoard(e), enemyGUIBoard(ge) {
-  aircraftCarrier = new AircraftCarrier(myBoard);
-  destroyer       = new Destroyer(myBoard)      ;
-  cruiser         = new Cruiser(myBoard)        ;
-  submarine       = new Submarine(myBoard)      ;
-  patrol          = new Patrol(myBoard)         ;
+
+SmartCPU::SmartCPU(Board* b, GUIBoard* gb, Board* e, GUIBoard* ge) : CPU("Smart CPU", b, gb, e, ge) {
+  startingPositions.push_back(Position(Coordinates(0, 0), Position::TOSOUTH));
+  startingPositions.push_back(Position(Coordinates(1, 0), Position::TOSOUTH));
+  startingPositions.push_back(Position(Coordinates(2, 0), Position::TOSOUTH));
+  startingPositions.push_back(Position(Coordinates(3, 0), Position::TOSOUTH));
+  startingPositions.push_back(Position(Coordinates(4, 0), Position::TOSOUTH));
+  startingPositions.push_back(Position(Coordinates(5, 0), Position::TOSOUTH));
+  startingPositions.push_back(Position(Coordinates(6, 0), Position::TOSOUTH));
+  startingPositions.push_back(Position(Coordinates(7, 0), Position::TOSOUTH));
+  startingPositions.push_back(Position(Coordinates(8, 0), Position::TOSOUTH));
+  startingPositions.push_back(Position(Coordinates(9, 0), Position::TOSOUTH));
+  startingPositions.push_back(Position(Coordinates(0, 0), Position::TOEAST));
+  startingPositions.push_back(Position(Coordinates(0, 1), Position::TOEAST));
+  startingPositions.push_back(Position(Coordinates(0, 2), Position::TOEAST));
+  startingPositions.push_back(Position(Coordinates(0, 3), Position::TOEAST));
+  startingPositions.push_back(Position(Coordinates(0, 4), Position::TOEAST));
+  startingPositions.push_back(Position(Coordinates(0, 5), Position::TOEAST));
+  startingPositions.push_back(Position(Coordinates(0, 6), Position::TOEAST));
+  startingPositions.push_back(Position(Coordinates(0, 7), Position::TOEAST));
+  startingPositions.push_back(Position(Coordinates(0, 8), Position::TOEAST));
+  startingPositions.push_back(Position(Coordinates(0, 9), Position::TOEAST));
 }
 
-User::~User() {
-  delete aircraftCarrier;
-  delete destroyer      ;
-  delete cruiser        ;
-  delete submarine      ;
-  delete patrol         ;
-}
-
-string User::getName() { return name; }
-
-void User::placeAllVessels() {
-  placeVessel(aircraftCarrier);
-  placeVessel(destroyer)      ;
-  placeVessel(cruiser)        ;
-  placeVessel(submarine)      ;
-  placeVessel(patrol)         ;
-}
-
-void User::placeVessel(Vessel* v) {
-  Position p = getPlacementPosition(v);
-  v->setPosition(p);
-  myBoard->placeVessel(v);
-}
-
-AttackResult User::takeTurn() {
-  Coordinates c = getAttackCoordinates();
-  AttackResult result = enemyBoard->attack(c);
-  switch(result.getState()) {
-  case AttackResult::MISS:
-    enemyGUIBoard->putc('M', c);
-    GUI::getInstance()->logEvent(name + " attacked at " + c.toString() + "; result : MISS.");
-    break;
-  case AttackResult::HIT:
-    enemyGUIBoard->putc('H', c);
-    GUI::getInstance()->logEvent(name + " attacked at " + c.toString() + "; result : HIT.");
-    break;
-  case AttackResult::SUNK:
-  case AttackResult::ALLSUNK:
-    Vessel* v = enemyBoard->getVesselAtCoordinates(c);
-    enemyGUIBoard->drawVessel(v);
-    GUI::getInstance()->logEvent(name + " attacked at " + c.toString() + "; result : SANK " + v->getName());
-  }
-  return result;
-}
-
-CPU::CPU(Board* b, GUIBoard* gb, Board* e, GUIBoard* ge) : User("CPU", b, gb, e, ge) {}
-
-Position CPU::getPlacementPosition(Vessel* v) {
-  Position p;
-  srand(time(NULL));
-  while(true) {
-    const int x = rand() % 10;
-    const int y = rand() % 10;
-    Coordinates c(x, y);
-
-    const int o = rand() % 4;
-    switch(o) {
-    case 0:
-      p = Position(c, Position::TONORTH);
-      break;
-    case 1:
-      p = Position(c, Position::TOEAST);
-      break;
-    case 2:
-      p = Position(c, Position::TOSOUTH);
-      break;
-    case 3:
-      p = Position(c, Position::TOWEST);
-      break;
+// Algorithm:
+// If there exist hit-and-not-sunk segments which are extensible,
+// - select the longest.
+// - return the attackable cell on either end.
+// If extensible hit-and-not-sunk segments don't exist, then
+// use the basic CPU's random algorithm:
+// - Randomly select a coordinates to be attacked.
+// - Make sure that this cell hasn't been attacked earlier.
+Coordinates SmartCPU::getAttackCoordinates() {
+  vector<Segment> extensibleSegments = filterExtensibleSegments(getHitSegments());
+  if(extensibleSegments.size() != 0) {
+    Coordinates attackedCell;
+    Segment s = getLongestExtensibleSegment(extensibleSegments);
+    if(canBeAttacked(s.getPreviousCell())) {
+      attackedCell = s.getPreviousCell();
     }
-    if(myBoard->isPositionAvailable(v, p)) {
-      GUI::getInstance()->logEvent(name + " placing " + v->getName() + " at " + p.toString() + ".");
-      return p;
-    } 
+    else {
+      attackedCell = s.getNextCell();
+    }
+    attackedCoordinates.push_back(attackedCell);
+    return attackedCell;
+  }
+  else {
+    return CPU::getAttackCoordinates();
   }
 }
 
-// Randomly select a coordinates to be attacked.
-// Make sure that this cell hasn't been attacked earlier.
-Coordinates CPU::getAttackCoordinates() {
-  while(true) {
-    const int x = rand() % 10;
-    const int y = rand() % 10;
-    Coordinates c(x, y);
-    // if the newly generated point hasn't been attacked so far,
-    // attack it.
-    if(find ( attackedCoordinates.begin(),
-              attackedCoordinates.end(),
-              c
-            ) == attackedCoordinates.end()) {
-      attackedCoordinates.push_back(c);
-      return c;
+// Of all the extensible hit-and-not-sunk segments, pick one of the longest ones.
+Segment SmartCPU::getLongestExtensibleSegment(vector<Segment> hitSegments) {
+  Segment max(Position(Coordinates(0, 0), Position::TOSOUTH), 0);
+  for(unsigned int i = 0; i < hitSegments.size(); i++) {
+    if(max.getLength() <= hitSegments[i].getLength()) {
+      max = hitSegments[i];
     }
   }
+  return max;
 }
 
-Player::Player(Board* b, GUIBoard* gb, Board* e, GUIBoard* ge) : User("Player", b, gb, e, ge) {}
-
-// Interact with the human player to place the given vessel v.
-// Start by placing the vessel at the top-left cell or whichever cell is next available to its
-// east.
-Position Player::getPlacementPosition(Vessel* v) {
-  Coordinates c = Coordinates(0, 0);
-  Position::Orientation o = Position::TOEAST;
-  Position p = Position(c, o);
-  v->setPosition(p);
-  if(!(myBoard->isPositionAvailable(v, p))) {
-    c = myBoard->nextAvailableCellToEast(v);
-  }
-  p = Position(c, o);
-  v->setPosition(p);
-  guiBoard->drawVessel(v);
-  while(true) {
-    int inp = (GUI::getInstance())->getKeyboard()->getKey();
-    switch(inp) {
-    case '\n':  // for ENTER
-    case 'q':
-      GUI::getInstance()->logEvent(name + " placing " + v->getName() + " at " + p.toString() + ".");
-      return v->getPosition();
-    case KEY_UP:
-      try {
-        Coordinates nc = myBoard->nextAvailableCellToNorth(v);
-        p = Position(nc, v->getPosition().getOrientation());
-        v->setPosition(p);
-        guiBoard->drawVessel(v);
-      }
-      catch(InvalidCoordinatesException e) {
-        GUI::getInstance()->logEvent("Can't move any further north.");  
-      }
-      break;
-    case KEY_DOWN:
-      try {
-        Coordinates nc = myBoard->nextAvailableCellToSouth(v);
-        p = Position(nc, v->getPosition().getOrientation());
-        v->setPosition(p);
-        guiBoard->drawVessel(v);
-      }
-      catch(InvalidCoordinatesException e) {
-        GUI::getInstance()->logEvent("Can't move any further south.");  
-      }
-      break;
-    case KEY_RIGHT:
-      try {
-        Coordinates nc = myBoard->nextAvailableCellToEast(v);
-        p = Position(nc, v->getPosition().getOrientation());
-        v->setPosition(p);
-        guiBoard->drawVessel(v);
-      }
-      catch(InvalidCoordinatesException e) {
-        GUI::getInstance()->logEvent("Can't move any further east.");  
-      }
-      break;
-    case KEY_LEFT:
-      try {
-        Coordinates nc = myBoard->nextAvailableCellToWest(v);
-        p = Position(nc, v->getPosition().getOrientation());
-        v->setPosition(p);
-        guiBoard->drawVessel(v);
-      }
-      catch(InvalidCoordinatesException e) {
-        GUI::getInstance()->logEvent("Can't move any further west.");  
-      }
-      break;
-    case 'r': // Rotate clockwise
-    {
-      p = v->getPosition().rotateCW();
-      if(myBoard->isPositionAvailable(v, p)) {
-        v->setPosition(p);
-        guiBoard->drawVessel(v);
+// Return the list of all the segments which are hit and not sunk on the enemy board.
+vector<Segment> SmartCPU::getHitSegments() {
+  vector<Segment> hitSegments;
+  for(unsigned int i = 0; i < startingPositions.size(); i++) {
+      vector<Segment> restOfIt;
+      if(isHitNotSunk(startingPositions[i].getCoordinates())) {
+        restOfIt.push_back(Segment(startingPositions[i], 0)); //state a new hit-segment
+        cellHit(restOfIt, startingPositions[i]);
       }
       else {
-        GUI::getInstance()->logEvent("Can't rotate clockwise. Position not available");
+        cellNotHit(restOfIt, startingPositions[i]);
       }
-      break;
-    }
-    case 'R': // Rotate counter-clockwise
-    {
-      p = v->getPosition().rotateCCW();
-      if(myBoard->isPositionAvailable(v, p)) {
-        v->setPosition(p);
-        guiBoard->drawVessel(v);
-      }
-      else {
-        GUI::getInstance()->logEvent("Can't rotate counter-clockwise. Position not available");
-      }
-      break;
-    }
-    default:
-      GUI::getInstance()->logEvent("Not a valid command.");
-      break; 
-    }
+      hitSegments.insert(hitSegments.end(), restOfIt.begin(), restOfIt.end());
   }
-  GUI::getInstance()->logEvent("placing " + v->getName() + " at " + v->getPosition().toString());
-  return v->getPosition();
+/*
+  ofstream fout;
+  fout.open("../segments.txt");
+  for(unsigned int i = 0; i < hitSegments.size(); i++) {
+    fout << hitSegments[i].toString() << endl;
+  }
+  fout.close();
+*/
+  return hitSegments;
 }
 
+// Return true if the cell is a valid one and hasn't been attacked.
+bool SmartCPU::canBeAttacked(Coordinates c) {
+  if(!enemyBoard->isWithinBoard(c)) return false;
+  if(isAttacked(c))                 return false;
+  return true;
+}
 
-// Interact with the human player to determine the cell on the CPU board that
-// will be attacked next.
-Coordinates Player::getAttackCoordinates() {
-  enemyGUIBoard->showCursor(true);
-  while(true) {
-    int inp = (GUI::getInstance())->getKeyboard()->getKey();
-    switch(inp) {
-    case '\n':  // for ENTER
-    case 'q':
-      return enemyGUIBoard->getCursorCoordinates();
-    case KEY_UP:
-      try {
-        enemyGUIBoard->moveCursor(Position::TONORTH);
-      }
-      catch(ActionNotAllowedException e) {
-        GUI::getInstance()->logEvent("Can't move any further north.");  
-      }
-      break;
-    case KEY_DOWN:
-      try {
-        enemyGUIBoard->moveCursor(Position::TOSOUTH);
-      }
-      catch(ActionNotAllowedException e) {
-        GUI::getInstance()->logEvent("Can't move any further south.");  
-      }
-      break;
-    case KEY_RIGHT:
-      try {
-        enemyGUIBoard->moveCursor(Position::TOEAST);
-      }
-      catch(ActionNotAllowedException e) {
-        GUI::getInstance()->logEvent("Can't move any further east.");  
-      }
-      break;
-    case KEY_LEFT:
-      try {
-        enemyGUIBoard->moveCursor(Position::TOWEST);
-      }
-      catch(ActionNotAllowedException e) {
-        GUI::getInstance()->logEvent("Can't move any further west.");  
-      }
-      break;
-    default:
-      break; 
+// If a segment has an attackable cell on either end, it is extensible.
+bool SmartCPU::isExtensible(Segment s) {
+    if(canBeAttacked(s.getPreviousCell())) return true;
+    if(canBeAttacked(s.getNextCell()))     return true;
+}
+
+// Out of the list of hit and not sunk segments, filter out those which are
+// extensible.
+vector<Segment> SmartCPU::filterExtensibleSegments(vector<Segment> segments) {
+  vector<Segment> extensibleSegments;
+  for(unsigned int i = 0; i < segments.size(); i++) {
+    if(isExtensible(segments[i])) {
+      extensibleSegments.push_back(segments[i]);
     }
+  }
+  return extensibleSegments;
+}
+
+// Return true if the given cell has been hit (i.e. has been attacked and there's a vessel there),
+// but the vessel there hasn't sunk.
+bool SmartCPU::isHitNotSunk(Coordinates coord) {
+  if(!isAttacked(coord)) {
+    return false;
+  }
+  Vessel *v = enemyBoard->getVesselAtCoordinates(coord);
+  if(v == NULL) {
+    return false;
+  }
+  if(v->getState() == Vessel::SUNK) {
+    return false;
+  }
+  return true;
+}
+
+// If exploring a column (orientation = TOSOUTH), then return true if at the last row.
+// If exploring a row (orientation = TOEAST), then return true if at the last column.
+bool SmartCPU::isLastPosition(Position p) {
+  Coordinates c = p.getCoordinates();
+  switch(p.getOrientation()) {
+  case Position::TOSOUTH:
+    return c.gety() == enemyBoard->getSize().gety();  // if last row
+  case Position::TOEAST:
+    return c.getx() == enemyBoard->getSize().getx();  // if last column
+  }
+}
+
+// If exploring a column (orientation = TOSOUTH), then return position to the south
+// If exploring a row (orientation = TOEAST), then return position to the east.
+Position SmartCPU::getNextPosition(Position p) {
+  Coordinates nextCell = p.getCoordinates();
+  switch(p.getOrientation()) {
+  case Position::TOSOUTH:
+    nextCell = p.getCoordinates() + Coordinates(0, 1); // next cell to the south
+    break;
+  case Position::TOEAST:
+    nextCell = p.getCoordinates() + Coordinates(1, 0); // next cell to the east
+    break;
+  }
+  return  Position(nextCell, p.getOrientation());
+}
+
+// State function when not in hit-segment.
+void SmartCPU::cellNotHit(vector<Segment>& segments, Position pos) {
+  if(isLastPosition(pos)) {
+    return;
+  }
+  Position nextPos = getNextPosition(pos);
+  if(isHitNotSunk(nextPos.getCoordinates())) { // going to enter a new hit-segment
+    segments.push_back(Segment(nextPos, 0)); //state a new hit-segment
+    cellHit(segments, nextPos);
+  }
+  else {
+    cellNotHit(segments, nextPos);
+  }
+}
+
+// State function when in hit-segment.
+void SmartCPU::cellHit(vector<Segment>& segments, Position pos) {
+  segments[segments.size() - 1].incrementLength();
+  if(isLastPosition(pos)) {
+    return;
+  }
+  Coordinates nextCell = pos.getCoordinates();
+  Position nextPos = getNextPosition(pos);
+  if(isHitNotSunk(nextPos.getCoordinates())) {  // continuing in the current hit-segment.
+    cellHit(segments, nextPos);
+  }
+  else {
+    cellNotHit(segments, nextPos);  // exit the hit-segment
   }
 }
